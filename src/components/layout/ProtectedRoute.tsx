13efc0b60bfedfbc1a1eedc01a560165eeb4bef8
hmsmiraz <hmsmiraz64729@gmail.com>
2024-07-09 15:03:57 +0600

    added 28 module

---
+++ src/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import { useCurrentToken } from '../../redux/features/auth/authSlice';
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from '../../redux/hook';
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = useAppSelector(useCurrentToken);
+
+  if (!token) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { RootState } from '../../store';
+import { RootState } from '../../store';
 
 type TAuthState = {
   user: null | object;
@@ -31,5 +31,5 @@ export const { setUser, logout } = authSlice.actions;
 
 export default authSlice.reducer;
 
-// export const useCurrentToken = (state: RootState) => state.auth.token;
-// export const useCurrentUser = (state: RootState) => state.auth.user;
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const useCurrentUser = (state: RootState) => state.auth.user;
