5470fc71a280d5da29e1a6728132d155d2c7a187
hmsmiraz <hmsmiraz64729@gmail.com>
2024-07-15 01:24:28 +0600

    added 31 module

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,7 +2,12 @@ import AdminDashboard from "../pages/Admin/AdminDashboard";
 import CreateAdmin from "../pages/Admin/CreateAdmin";
 import CreateFaculty from "../pages/Admin/CreateFaculty";
 import CreateStudent from "../pages/Admin/CreateStudent";
+import AcademicDepartment from "../pages/Admin/academicManagement/AcademicDepartment";
+import AcademicFaculty from "../pages/Admin/academicManagement/AcademicFaculty";
 import AcademicSemester from "../pages/Admin/academicManagement/AcademicSemester";
+import CreateAcademicDepartment from "../pages/Admin/academicManagement/CreateAcademicDepartment";
+import CreateAcademicFaculty from "../pages/Admin/academicManagement/CreateAcademicFaculty";
+import CreateAcademicSemester from "../pages/Admin/academicManagement/CreateAcademicSemester";
 
 
 
@@ -16,9 +21,34 @@ export const adminPaths = [
     name: 'Academic Management',
     children: [
       {
+        name: 'Create A. Semester',
+        path: 'create-academic-semester',
+        element: <CreateAcademicSemester />,
+      },
+      {
         name: 'Academic Semester',
         path: 'academic-semester',
         element: <AcademicSemester />,
+      },
+      {
+        name: 'Create A. Faculty',
+        path: 'create-academic-faculty',
+        element: <CreateAcademicFaculty />,
+      },
+      {
+        name: 'Academic Faculty',
+        path: 'academic-faculty',
+        element: <AcademicFaculty />,
+      },
+      {
+        name: 'Create A. Department',
+        path: 'create-academic-department',
+        element: <CreateAcademicDepartment />,
+      },
+      {
+        name: 'Academic Department',
+        path: 'academic-department',
+        element: <AcademicDepartment />,
       },
     ],
   },
